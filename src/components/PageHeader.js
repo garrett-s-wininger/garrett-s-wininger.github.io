ce75888f4754a8d91378cdc05648ff8d758fa011
Garrett Wininger <garrett.s.wininger@outlook.com>
2021-07-22 22:10:23 -0400

    Removed additional header space usage due to translation

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,27 +3,35 @@ import './pageHeader.css';
 import Image from './Image';
 import * as React from 'react';
 
+const profileBackgroundHeightRem: number = 9;
+const profilePictureBorderWidthRem: number = 0.25;
+const profilePictureAxisRem: number = 8;
+const profilePictureYTranslationPercentage: number = 35;
+
 const styles = {
+    profileHeader: {
+        height: `calc(${profileBackgroundHeightRem}rem + (${profilePictureAxisRem}rem * ${(100 - profilePictureYTranslationPercentage) / 100}) + ${profilePictureBorderWidthRem}rem)`
+    },
     profileBackground: {
-        height: '9rem',
+        height: `${profileBackgroundHeightRem}rem`,
         width: '100%'
     },
     profilePicture: {
         borderColor: 'white',
         borderRadius: '50%',
         borderStyle: 'solid',
-        borderWidth: '0.25rem',
-        height: '8rem',
+        borderWidth: `${profilePictureBorderWidthRem}rem`,
+        height: `${profilePictureAxisRem}rem`,
         marginLeft: 'auto',
         marginRight: 'auto',
-        transform: 'translate(0, -35%)',
-        width: '8rem'
+        transform: `translate(0, -${profilePictureYTranslationPercentage}%)`,
+        width: `${profilePictureAxisRem}rem`
     }
 };
 
 const PageHeader = (): React.MixedElement => {
     return (
-        <header className="pageHeader">
+        <header className="pageHeader" style={styles.profileHeader}>
             <Image source="/profile_background_light.png" altText="Arch Linux" style={styles.profileBackground} />
             <Image source="/profile.png" altText="Garrett Wininger" style={styles.profilePicture} />
         </header>
